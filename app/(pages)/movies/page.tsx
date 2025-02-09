e91d2b989ddf9f9e0745d3c45c4ffdbbd0146152
andre mcmorris <ajmcmorris@gmail.com>
2025-02-09 12:55:34 -0500

    updated types/updated shows page

--- app/(pages)/movies/page.tsx
+++ app/(pages)/movies/page.tsx
@@ -1,11 +1,12 @@
 import { Card } from "@/app/components/card/card";
 import { SearchBar } from "@/app/components/searchbar/searchbar";
 import { getMovies } from "@/app/lib/data";
+import { Movie } from "@/app/lib/types";
 
 export default async function Movies({ searchParams }: { searchParams: { query?: string } }) {
     const  movies = await getMovies();
     const query = searchParams?.query || "";
-    const filteredMovies = movies.filter((movie: any) => movie.title.toLowerCase().includes(query.toLowerCase()));
+    const filteredMovies = movies.filter((movie: Movie) => movie.title.toLowerCase().includes(query.toLowerCase()));
     
     return(
         <>
@@ -13,7 +14,7 @@ export default async function Movies({ searchParams }: { searchParams: { query?:
             <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 <h2 className="text-4xl font-extrabold dark:text-white">Search Results</h2>
                 <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    {filteredMovies.map((movie: any) => (
+                    {filteredMovies.map((movie: Movie) => (
                         <Card data={movie} key={movie.id} />
                     ))}
                 </div>
--- app/(pages)/tvshows/page.tsx
+++ app/(pages)/tvshows/page.tsx
@@ -1,12 +1,24 @@
+import { Card } from "@/app/components/card/card";
 import { SearchBar } from "@/app/components/searchbar/searchbar";
+import { getShows } from "@/app/lib/data";
+import { TvShow } from "@/app/lib/types";
 
 export default async function TvShows({ searchParams }: { searchParams: { query?: string } }) {
+    const shows = await getShows();
     const query = searchParams?.query || "";
+    const filteredShows = shows.filter((show: TvShow) => show.name.toLowerCase().includes(query.toLowerCase()));
     
     return(
         <>
-            <h2 className="text-4xl font-extrabold dark:text-white">Tv Show Page</h2>
-            <SearchBar placeholder="Search for media" />
-        </>
+                    <SearchBar placeholder="Search for media" />
+                    <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                        <h2 className="text-4xl font-extrabold dark:text-white">Search Results</h2>
+                        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                            {filteredShows.map((show: TvShow) => (
+                                <Card data={show} key={show.id} />
+                            ))}
+                        </div>
+                    </section>
+                </>
     )
 }
